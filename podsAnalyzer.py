57187ccbc04b34e069377448d2ff2b4507c47d96
Longo97 <marco.longobardi997@gmail.com>
2023-09-27 12:25:01 +0200

    Update podsAnalyzer.py

--- podsAnalyzer.py
+++ podsAnalyzer.py
@@ -3,17 +3,16 @@ import subprocess
 import plistlib
 import json
 
-# Extract the pod name and version from the line
+# Function to extract the pod name and version from the line
 def extract_pod_info(line):
-    match = re.match(r'^\s*-\s+([^\s]+)\s+\(([^)]+)\)', line)
+    match = re.match(r'^\s*-\s+"?([^/\s]+)\s+\(([^)]+)\)', line)
     if match:
         return match.group(1), match.group(2)
     return None, None
 
-# Get license and git source information for the pod
+# Function to get license and git source information for the pod
 def get_pod_info(pod_name):
     try:
-        print("Analyzing...", str(pod_name))
         # Run the 'pod spec cat' command to get pod information
         result = subprocess.run(['pod', 'spec', 'cat', pod_name], capture_output=True, text=True)
         if result.returncode == 0:
@@ -30,24 +29,32 @@ def get_pod_info(pod_name):
 # Path to the Podfile.lock file
 file_path = 'Podfile.lock'
 
-# Dictionary to store pod names and information
+# Dictionary to store pod names, versions, license, and git source information
 pod_info_dict = {}
+
+# Set to keep track of processed pod names
+processed_pods = set()
 
 # Read the file and extract information
 with open(file_path, 'r') as file:
     lines = file.readlines()
-    current_pod = None
 
     for line in lines:
-        if line.startswith('    - '):
-            pod_name, pod_version = extract_pod_info(line)
-            if pod_name and pod_version:
-                license, git_source = get_pod_info(pod_name)
-                pod_info_dict[pod_name] = {
-                    'version': pod_version,
-                    'license': license,
-                    'git_source': git_source
-                }
+        pod_name, pod_version = extract_pod_info(line)
+        if pod_name and pod_name not in processed_pods:
+            print("Analyzing...", str(pod_name))
+            # Get license and git source information
+            license, git_source = get_pod_info(pod_name)
+            
+            # Add pod information to the dictionary
+            pod_info_dict[pod_name] = {
+                'version': pod_version,
+                'license': license,
+                'git_source': git_source
+            }
+
+            # Add the pod name to the set of processed pods
+            processed_pods.add(pod_name)
 
 # Write the information to a plist file
 output_plist_path = 'pod_info.plist'
