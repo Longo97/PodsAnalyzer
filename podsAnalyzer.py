69bd128d7896da1d1550e4691a345a80e4a9cd8a
Longo97 <marco.longobardi997@gmail.com>
2023-09-27 11:08:32 +0200

    Updated info retrieval

--- podsAnalyzer.py
+++ podsAnalyzer.py
@@ -1,47 +1,57 @@
-import os
+import re
 import subprocess
-import json
 import plistlib
+import json
 
+# Extract the pod name and version from the line
+def extract_pod_info(line):
+    match = re.match(r'^\s*-\s+([^\s]+)\s+\(([^)]+)\)', line)
+    if match:
+        return match.group(1), match.group(2)
+    return None, None
 
-# Get the path of the directory where the script is located
-base_folder = os.path.dirname(os.path.abspath(__file__))
+# Get license and git source information for the pod
+def get_pod_info(pod_name):
+    try:
+        print("Analyzing...", str(pod_name))
+        # Run the 'pod spec cat' command to get pod information
+        result = subprocess.run(['pod', 'spec', 'cat', pod_name], capture_output=True, text=True)
+        if result.returncode == 0:
+            # Parse the result as JSON
+            pod_info = json.loads(result.stdout)
+            license = pod_info.get('license', 'Not specified')
+            git_source = pod_info.get('source', {}).get('git', 'Not specified')
+            return license, git_source
+        else:
+            return 'Error retrieving information', ''
+    except Exception as e:
+        return 'Error retrieving information', str(e)
 
-# A list to store information for each subfolder
-info_list = []
+# Path to the Podfile.lock file
+file_path = 'Podfile.lock'
 
-subfolders = [f.name for f in os.scandir(base_folder + "/Pods") if f.is_dir()]
+# Dictionary to store pod names and information
+pod_info_dict = {}
 
-# # Iterate over each subfolder and execute the pod spec command
-for subfolder in subfolders:
+# Read the file and extract information
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+    current_pod = None
 
-    command = f"pod spec cat {subfolder}"
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print("analyzing..", str(subfolder))
+    for line in lines:
+        if line.startswith('    - '):
+            pod_name, pod_version = extract_pod_info(line)
+            if pod_name and pod_version:
+                license, git_source = get_pod_info(pod_name)
+                pod_info_dict[pod_name] = {
+                    'version': pod_version,
+                    'license': license,
+                    'git_source': git_source
+                }
 
-    # Check the result
-    if result.returncode == 0:
-        # The command was executed successfully, parse the JSON output
-        try:
-            json_output = json.loads(result.stdout)
-            info = {
-                "name": json_output["name"],
-                "version": json_output["version"],
-                "license": json_output["license"]
-            }
+# Write the information to a plist file
+output_plist_path = 'pod_info.plist'
+with open(output_plist_path, 'wb') as output_file:
+    plistlib.dump(pod_info_dict, output_file)
 
-            git_source = json_output["source"].get("git", "N/A")
-            info["git_source"] = git_source
-            info_list.append(info)
-
-        except json.JSONDecodeError as e:
-            print("Cannot be analyzed:", str(subfolder))
-    else:
-        print("Error during command execution:", str(subfolder))
-
-# plist file creation
-plist_file_path = "info_pods.plist"
-with open(plist_file_path, 'wb') as plist_file:
-    plistlib.dump(info_list, plist_file)
-
-print("plist file created successfully:", plist_file_path)
+print(f'Pod information written to: {output_plist_path}')
